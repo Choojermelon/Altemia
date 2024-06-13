b9b53ad75451313f3e51cde468bb437304ff8451
Chuji <64228911+Choojermelon@users.noreply.github.com>
2024-06-13 01:05:33 +0100

    Quartz sync: Jun 13, 2024, 1:05 AM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,17 +14,20 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <hr />
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
+		<p>
+			Lovingly created and maintained by Chuji :3
+		</p>
+        // <p>
+          // {i18n(cfg.locale).components.footer.createdWith}{" "}
+          // <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+        // </p>
+        // <ul>
+          // {Object.entries(links).map(([text, link]) => (
+            // <li>
+              // <a href={link}>{text}</a>
+            // </li>
+          // ))}
+        // </ul>
       </footer>
     )
   }
